fe12b1f0d6e6604b1ef9d4a98623b68df6f1214f
pcaliman2 <pcaliman@gmail.com>
2025-08-15 13:33:52 -0400

    Cambio en el Waitres

--- src/run_waitress.py
+++ src/run_waitress.py
@@ -1,7 +1,11 @@
-from waitress import serve
-from app import app
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from waitress import serve
+
+# Asegurar que el directorio actual esté en el path
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+
+from app import app  # Importar después de modificar sys.path
 
 serve(app, host='0.0.0.0', port=5000)
